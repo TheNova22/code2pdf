61e4e5312a8aaf3edc5ec9b77444499e26cc3234
TheNova22 <jayantsogikar@gmail.com>
2022-10-19 01:21:27 +0530

    File consumer added

--- file_consumer/file_consumer.py
+++ file_consumer/file_consumer.py
@@ -4,26 +4,21 @@ from  pygments import lexers
 from cassandra.cluster import Cluster
 from cassandra.util import max_uuid_from_time
 from kafka import KafkaConsumer
+import os
 
-
-cluster = Cluster(['cassandra'], port = 9042, idle_heartbeat_interval=10)
+cluster = Cluster(['cassandra'], port = 9042, idle_heartbeat_interval=10 )
 session = cluster.connect()
 session.default_timeout = 100
 session.set_keyspace('code2pdf')
-consumer = KafkaConsumer('url',
+consumer = KafkaConsumer('file',
                          bootstrap_servers=['kafka:29092'])
 
-consumer.subscribe(['url'])
+consumer.subscribe(['file'])
 
 for message in consumer:
     m = message.value.decode('utf-8')
-    print(m)
     d = json.loads(m)
     print(d)
-    lex = lexers.guess_lexer_for_filename(d["url"].split('/')[-1],'int a = 3;')
+    lex = lexers.guess_lexer_for_filename(d["file"],'int a = 3;')
     session.execute("""INSERT INTO codelang (usn, tid, lang) values (%(usn)s, %(tid)s, %(lang)s)""", {"usn" : d["usn"], "tid" : max_uuid_from_time(datetime.now()), "lang" : lex.name})
-    # x = session.execute("INSERT INTO codelang (usn, tid, lang) values({0},{1},{2})".format(d["usn"],max_uuid_from_time(datetime.now()), lex.name))
-# x = session.execute("DESC tables")
-
-# for r in x:
-#     print("Hello " + str(r))
+    os.remove("/volume/" + d["file"])
